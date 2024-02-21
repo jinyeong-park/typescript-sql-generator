342bc50ab5871d5103be151e5e26f2bf50cd37e7
Jinyeong <jinyeongpark1205@gamil.com>
2024-02-21 12:13:09 -0800

    basic files

---
+++ src/components/CodeDisplay.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CodeDisplay = () => {
+  return <div>CodeDisplay</div>;
+};
+
+export default CodeDisplay;
---
+++ src/components/MessageDisplay.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MessageDisplay = () => {
+  return <div>MessageDisplay</div>;
+};
+
+export default MessageDisplay;
